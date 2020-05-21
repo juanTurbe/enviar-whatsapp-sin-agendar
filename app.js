2e6e627e3c2384af8d50282450a4fa4b6d0011b2
Juan Iturbe <juaniturbe16@gmail.com>
2020-05-21 18:11:10 -0300

    valide formulario y corregi estilos

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 let valorNumero = document.getElementById('numero'),
-btnEnviar = document.getElementById('btn-numero');
-const enlace = document.createElement('a');
+btnEnviar = document.getElementById('btn-numero'),
+enlace = document.createElement('a'),
+aviso = document.createElement('p');
+
+
 
 btnEnviar.addEventListener('click', function(){
-    enlace.className = 'btn-enviar';
-    enlace.setAttribute('href', 'https://api.whatsapp.com/send?phone=' + valorNumero.value);
-    enlace.setAttribute('target', '_blank');
-    enlace.textContent = 'Enviar por Whatsapp';
-    document.querySelector('.botones').appendChild(enlace);
-});    
+    if (valorNumero.value === "" ) {
+        aviso.style.color = "red";
+        aviso.textContent = 'Ingresa un número';
+        document.querySelector('.lista').appendChild(aviso);
+    } else {
+        aviso.style.display = "none";
+        enlace.className = 'btn-enviar';
+        enlace.setAttribute('href', 'https://api.whatsapp.com/send?phone=' + valorNumero.value);
+        enlace.setAttribute('target', '_blank');
+        enlace.textContent = 'Enviar por Whatsapp';
+        document.querySelector('.botones').appendChild(enlace);
+    }
+
+    enlace.addEventListener('click', function(){
+        location.reload();
+    });
+});
